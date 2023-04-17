6f6573681df6cd84109601b38b6652a8b48bc060
ifeanyi222 <linusify@ymail.com>
2023-04-17 13:33:14 +0100

    0-constants.js

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+function taskFirst() {
+  const task = 'I prefer const when I can.';
+  return task;
+}
+
+function getLast() {
+  return ' is okay';
+}
+
+function taskNext() {
+  let combination = 'But sometimes let';
+  combination += getLast();
+
+  return combination;
+}
